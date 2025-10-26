03cead0962afd9c63644a3a885b3e3b273e80bc8
Jeffrey Friedman <20387583+jeffreyfriedman@users.noreply.github.com>
2025-10-25 22:11:29 -0400

    fix(compiler): Fix import resolution and method call codegen
    Fixed three critical codegen issues:
    
    1. Import Resolution - Type vs Module Detection
    2. Braced Imports - Proper Formatting
    3. Method Calls on Fields - Instance vs Static
    
    Progress: 6/25 TODOs complete

--- build/ui_counter_simple.rs
+++ build/ui_counter_simple.rs
@@ -2,9 +2,9 @@
 
 
 
-use windjammer_ui::reactivity::Signal::*;
+use windjammer_ui::reactivity::Signal;
 
-use windjammer_ui::vdom::{VElement, VNode, VText}::*;
+use windjammer_ui::vdom::{VElement, VNode, VText};
 
 use windjammer_ui::wasm_events::*;
 
@@ -20,7 +20,7 @@ fn new() -> CounterApp {
 }
 #[inline]
 fn render(self) -> VNode {
-        let count_value = self.count::get();
+        let count_value = self.count.get();
         VNode::Element(VElement::new("div").attr("class", "counter").child(VNode::Element(VElement::new("h1").child(VNode::Text(VText::new(format!("Count: {}", count_value)))))).child(VNode::Element(VElement::new("button").attr("id", "increment").child(VNode::Text(VText::new("+"))))))
 }
 }
